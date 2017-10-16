better tests; work on backprop; fixed some bugs

loopy/models/backprop.py
@@ -2,17 +2,27 @@ import numpy as np
 import random
 import loopy
 
-node_memory_size = 3
-edge_memory_size = 3
-# node_memory: forward_signal, forward_signal_sent, error_signal_sent
-# edge_memory: signal (not multiplied by weight), error, weight
+import logging
+logger = logging.getLogger()
 
-NODE_FORWARD_SIGNAL_INDEX = 0
-NODE_FORWARD_SIGNAL_SENT_INDEX = 1
-NODE_ERROR_SIGNAL_SENT_INDEX = 2
+node_memory_size = 3
+edge_memory_size = 5
+# node_memory: signal_memory, signal_just_sent, error_just_sent
+# edge_memory: signal (not multiplied by weight), has_signal, error, has_error, weight
+
+# some help for thinking about error...
+# error = (expected_signal - actual_signal) * gradient
+
+NODE_SIGNAL_MEMORY_INDEX = 0
+NODE_SIGNAL_JUST_SENT_INDEX = 1
+NODE_ERROR_JUST_SENT_INDEX = 2
+
 EDGE_SIGNAL_INDEX = 0
-EDGE_ERROR_INDEX = 1
-EDGE_WEIGHT_INDEX = 2
+EDGE_HAS_SIGNAL_INDEX = 1
+EDGE_ERROR_INDEX = 2
+EDGE_HAS_ERROR_INDEX = 3
+EDGE_WEIGHT_INDEX = 4
+
 
 from loopy.graph.feedforward import make_feedforward_adjacency_dict
 from loopy.network import Network
@@ -20,27 +30,82 @@ from loopy.network import Network
 
 def backprop_initialize_rule(node_memory_size, edge_memory_size, edges):
     node_buffer = np.zeros(node_memory_size + edge_memory_size * edges)
-    node_buffer[NODE_FORWARD_SIGNAL_INDEX] = 0.0
-    node_buffer[NODE_FORWARD_SIGNAL_SENT_INDEX] = 0.0
-    node_buffer[NODE_ERROR_SIGNAL_SENT_INDEX] = 0.0
+    node_buffer[NODE_SIGNAL_MEMORY_INDEX] = 0
+    node_buffer[NODE_SIGNAL_JUST_SENT_INDEX] = 0
+    node_buffer[NODE_ERROR_JUST_SENT_INDEX] = 0
     node_buffer[node_memory_size + EDGE_SIGNAL_INDEX::edge_memory_size] = 0.0
+    node_buffer[node_memory_size + EDGE_HAS_SIGNAL_INDEX::edge_memory_size] = 0
     node_buffer[node_memory_size + EDGE_ERROR_INDEX::edge_memory_size] = 0.0
+    node_buffer[node_memory_size + EDGE_HAS_ERROR_INDEX::edge_memory_size] = 0
     node_buffer[node_memory_size + EDGE_WEIGHT_INDEX::edge_memory_size] = random.random() - 0.5
     return node_buffer
 
 
+LEAKY_RELU_LEFT_SIDE_COEFFICIENT = 0.1
+LEARNING_RATE = 1e-3
 def backprop_step_rule(node_read_buffer, node_write_buffer, node_memory_size, edge_memory_size, edges):
-    return NotImplemented
+    node_write_buffer[:] = node_read_buffer
+
+    if node_read_buffer[NODE_SIGNAL_JUST_SENT_INDEX] == 0 and (node_read_buffer[node_memory_size + EDGE_HAS_SIGNAL_INDEX::edge_memory_size] != 0).any():
+        # trigger on any incoming signals
+        # part of forward pass
+        node_signal = 0.0
+
+        for neighbor in range(edges):
+            edge_memory = node_read_buffer[node_memory_size + neighbor * edge_memory_size:node_memory_size + (neighbor + 1) * edge_memory_size]
+            if edge_memory[EDGE_HAS_SIGNAL_INDEX] != 0:
+                # this edge has a signal
+                node_signal += edge_memory[EDGE_SIGNAL_INDEX] * edge_memory[EDGE_WEIGHT_INDEX]
+            else:
+                # this edge does not have a signal
+                pass
+
+        # leaky ReLU
+        if node_signal < 0.0:
+            node_signal = LEAKY_RELU_LEFT_SIDE_COEFFICIENT * node_signal
+
+        for neighbor in range(edges):
+            edge_memory = node_read_buffer[node_memory_size + neighbor * edge_memory_size:node_memory_size + (neighbor + 1) * edge_memory_size]
+            if edge_memory[EDGE_HAS_SIGNAL_INDEX] != 0:
+                # this edge has a signal
+                edge_memory[EDGE_HAS_SIGNAL_INDEX] = 0
+            else:
+                # this edge does not have a signal
+                edge_memory[EDGE_SIGNAL_INDEX] = node_signal
+                edge_memory[EDGE_HAS_SIGNAL_INDEX] = 1
+
+        node_write_buffer[NODE_SIGNAL_MEMORY_INDEX] = node_signal
+        node_write_buffer[NODE_SIGNAL_JUST_SENT_INDEX] = 1
+        return
+
+    if node_read_buffer[NODE_SIGNAL_JUST_SENT_INDEX] == 1:
+        # trigger if we sent a signal last step
+        # part of forward pass
+        node_write_buffer[NODE_SIGNAL_JUST_SENT_INDEX] = 0
+        node_write_buffer[node_memory_size + EDGE_SIGNAL_INDEX::edge_memory_size] = 0
+        node_write_buffer[node_memory_size + EDGE_HAS_SIGNAL_INDEX::edge_memory_size] = 0
+        return
+
+    if (node_read_buffer[node_memory_size + EDGE_HAS_ERROR_INDEX::edge_memory_size] != 0).any():
+        # trigger on any incoming errors
+        # part of backward pass
+
+        actual_signal = node_read_buffer[NODE_SIGNAL_MEMORY_INDEX]
+        if actual_signal < 0.0:
+            gradient = LEAKY_RELU_LEFT_SIDE_COEFFICIENT
+        else:
+            gradient = 1.0
+
+        pass
+
 
 
 class BackpropModel:
-    LEARNING_RATE = 1e-3
-    
     def __init__(self, input_size=2, hidden_size=1, output_size=1, layers=1):
         if layers != 1:
             raise Exception('BackpropModel does not support layers != 1 just yet')
-        self.network = Network(in_adjacency_dict=make_feedforward_adjacency_dict(input_size=input_size, hidden_size=hidden_size, output_size=output_size, layers=layers), node_memory_size=3, edge_memory_size=3)
-        network.initialize(initialize_rule=backprop_initialize_rule)
+        self.network = Network(in_adjacency_dict=make_feedforward_adjacency_dict(input_size=input_size, hidden_size=hidden_size, output_size=output_size, secondary_output_layer=True, layers=layers), node_memory_size=node_memory_size, edge_memory_size=edge_memory_size, step_rule=backprop_step_rule)
+        self.network.initialize(initialize_rule=backprop_initialize_rule)
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
@@ -48,7 +113,7 @@ class BackpropModel:
 
     def train(self, dataset, iterations=100):
         for iteration in range(iterations):
-            input_data, output_data = random.choice(train_dataset)
+            input_data, output_data = random.choice(dataset)
             self.async_train(input_data, output_data)
 
     def clean(self):
@@ -56,17 +121,20 @@ class BackpropModel:
         for node_buffer in self.network.read_buffer:
             node_memory = node_buffer[:node_memory_size]
             edge_memory = node_buffer[node_memory_size:]
-            node_memory[NODE_FORWARD_SIGNAL_INDEX] = 0.0
-            node_memory[NODE_FORWARD_SIGNAL_SENT_INDEX] = 0.0
-            node_memory[NODE_ERROR_SIGNAL_SENT_INDEX] = 0.0
+            node_memory[NODE_SIGNAL_MEMORY_INDEX] = 0
+            node_memory[NODE_SIGNAL_JUST_SENT_INDEX] = 0
+            node_memory[NODE_ERROR_JUST_SENT_INDEX] = 0
             edge_memory[EDGE_SIGNAL_INDEX] = 0.0
+            edge_memory[EDGE_HAS_SIGNAL_INDEX] = 0
             edge_memory[EDGE_ERROR_INDEX] = 0.0
-            # dont reset weights
+            edge_memory[EDGE_HAS_ERROR_INDEX] = 0
+            # don't reset weights
 
     def forward(self, input_data):
         # set input data
         for node, signal in enumerate(input_data):
-            self.network.read_buffer[node][NODE_FORWARD_SIGNAL_INDEX] = signal
+            self.network.read_buffer[node][NODE_SIGNAL_MEMORY_INDEX] = signal
+            self.network.read_buffer[node][NODE_SIGNAL_JUST_SENT_INDEX] = signal
             for neighbor in self.network.adjacency_dict[node]:
                 edge_memory = np.zeros(edge_memory_size)
                 edge_memory[EDGE_SIGNAL_INDEX] = signal
@@ -76,14 +144,26 @@ class BackpropModel:
         # note that right now multiple input signals coming in out of sync would make the network send output data along input edges
         # so don't try doing anything fancy to handle out of sync signals, since there's no way around this unfortunate fact
 
-        for layer in range(self.layers + 2):
+        # wait for all secondary_output_nodes to have NODE_SIGNAL_JUST_SENT_INDEX
+
+        secondary_output_nodes = list(range(len(self.network.nodes) - self.output_size, len(self.network.nodes)))
+
+        steps = 0
+        while True:
+            self.network.debug_log_buffers('step={}'.format(steps))
             self.network.step()
+            ready = all(self.network.get_node_memory(output_node)[NODE_SIGNAL_JUST_SENT_INDEX] == 1 for output_node in secondary_output_nodes)
+            if ready:
+                break
+            steps += 1
+            if steps > 1000:
+                logger.error('ERROR: ran > 1000 steps in a forward pass without getting an output; probably a bug!')
+                raise Exception('ran > 1000 steps in forward pass without getting an output')
 
         output = []
-        output_nodes = list(range(len(self.network.nodes) - self.output_size, len(self.network_nodes)))
 
-        for output_node in output_nodes:
-            output.append(self.network.read_buffer[output_node][NODE_FORWARD_SIGNAL_INDEX])
+        for output_node in secondary_output_nodes:
+            output.append(self.network.read_buffer[output_node][NODE_SIGNAL_MEMORY_INDEX])
 
         return output
 
@@ -91,6 +171,14 @@ class BackpropModel:
         # apply error to the last nodes/edges
         # then run step a few times
         return NotImplemented
+
+    def initialize_weights(self, weights):
+        # weights = {(node_a, node_b): weight, ...}
+        self.clean()
+        for edge, weight in weights.items():
+            edge_memory = np.zeros(edge_memory_size)
+            edge_memory[EDGE_WEIGHT_INDEX] = weight
+            self.network.set_edge_memory(edge, edge_memory)
 
     def async_train(self, input_data, output_data):
         self.clean()

loopy/tests/test_network.py
@@ -14,33 +14,68 @@ from loopy.network import Network
 from loopy.graph.waxman import make_waxman_adjacency_dict
 from loopy.graph.loop import make_loop_adjacency_dict
 
+unittest.run_fast = False
 
 def basic_initialize_rule(node_memory_size, edge_memory_size, edges):
     return np.zeros(node_memory_size + edge_memory_size * edges)
 
 
 def basic_step_rule(node_read_buffer, node_write_buffer, node_memory_size, edge_memory_size, edges):
-    node_write_buffer[:] = node_read_buffer[:]
-    node_write_buffer[node_memory_size:] = 0
+    # this step_rule just propagates multidimensional signals around on edges
+    # it averages them together
 
-    if node_read_buffer[0] > 0.1:
-        node_write_buffer[0] /= 2.0
-        return
+    # it ignores node_memory[1:] and edge_memory[2:] - so does not test for different node_memory_size / edge_memory_size
+
+    NODE_SIGNAL_JUST_SENT_INDEX = 0
+
+    EDGE_HAS_SIGNAL_INDEX = 0
+    EDGE_SIGNAL_INDEX = 1
+
+    node_write_buffer[:] = node_read_buffer[:]
 
     edge_read_memory = node_read_buffer[node_memory_size:]
     edge_write_memory = node_write_buffer[node_memory_size:]
 
-    output = np.zeros(edge_memory_size)
-    for neighbor in range(edges):
-        output += edge_read_memory[neighbor * edge_memory_size: (neighbor + 1) * edge_memory_size]
+    if node_read_buffer[NODE_SIGNAL_JUST_SENT_INDEX] != 0:
+        # we just sent a signal last step; don't do anything this step
+        node_write_buffer[NODE_SIGNAL_JUST_SENT_INDEX] = 0
+        edge_write_memory[EDGE_SIGNAL_INDEX::edge_memory_size] = 0
+        edge_write_memory[EDGE_HAS_SIGNAL_INDEX::edge_memory_size] = 0
+        return
+
+
+    if (edge_read_memory[EDGE_HAS_SIGNAL_INDEX::edge_memory_size] == 0).all() and node_read_buffer[NODE_SIGNAL_JUST_SENT_INDEX] == 0:
+        # we have no incoming signals and we didn't sent any signals last step
+        node_write_buffer[NODE_SIGNAL_JUST_SENT_INDEX] = 0
+        edge_write_memory[EDGE_SIGNAL_INDEX::edge_memory_size] = 0
+        edge_write_memory[EDGE_HAS_SIGNAL_INDEX::edge_memory_size] = 0
+        return
+
+    # if we reach here that means we have an incoming signal and need to propagate it!
+    total_input = 0.0
+    number_inputs = 0
+
+    node_write_buffer[NODE_SIGNAL_JUST_SENT_INDEX] = 1.0
 
     for neighbor in range(edges):
-        neighbor_edge_read_memory = edge_read_memory[neighbor * edge_memory_size: (neighbor + 1) * edge_memory_size]
-        neighbor_edge_write_memory = edge_write_memory[neighbor * edge_memory_size: (neighbor + 1) * edge_memory_size]
-        neighbor_edge_write_memory[:] = output * (abs(neighbor_edge_read_memory) <= 0.01)
+        if edge_read_memory[neighbor*edge_memory_size + EDGE_HAS_SIGNAL_INDEX] != 0:
+            number_inputs += 1
+            total_input += edge_read_memory[neighbor*edge_memory_size + EDGE_SIGNAL_INDEX]
 
-    if any(node_write_buffer[node_memory_size:]):
-        node_write_buffer[0] = 1.0
+    output = total_input*2.0 / number_inputs
+    # times 2.0 is a really weird requirement here: it's because what we assign an edge gets averaged with what the other node connected to it assigns it; so we multiply by 2 to indicate this is the actual value we want
+    # this is a place where a geometric average would be nicer; we're able to indicate if we want to turn it off (set edge memory to 0) or just don't care what the output is (set edge memory to 1)
+    # but life becomes complicated; square roots :'X
+    # and you still need to set edges to 2 to indicate presence of a signal... since 1*1=1 ...
+
+    for neighbor in range(edges):
+        if edge_read_memory[neighbor*edge_memory_size + EDGE_HAS_SIGNAL_INDEX] == 0:
+            edge_write_memory[neighbor*edge_memory_size + EDGE_HAS_SIGNAL_INDEX] = 2
+            edge_write_memory[neighbor*edge_memory_size + EDGE_SIGNAL_INDEX] = output
+        else:
+            edge_write_memory[neighbor*edge_memory_size + EDGE_HAS_SIGNAL_INDEX] = 0
+            edge_write_memory[neighbor*edge_memory_size + EDGE_SIGNAL_INDEX] = 0
+            # make it have no signal
 
 
 class TestNetwork(unittest.TestCase):
@@ -48,63 +83,70 @@ class TestNetwork(unittest.TestCase):
         Network(in_adjacency_dict=make_loop_adjacency_dict())
 
     def test_initialize(self):
-        network = Network(in_adjacency_dict=make_loop_adjacency_dict(), node_memory_size=1, edge_memory_size=1)
+        network = Network(in_adjacency_dict=make_loop_adjacency_dict(), node_memory_size=1, edge_memory_size=2)
         network.initialize(initialize_rule=basic_initialize_rule)
 
     def test_step(self):
-        network = Network(in_adjacency_dict=make_loop_adjacency_dict(), node_memory_size=1, edge_memory_size=1, step_rule=basic_step_rule)
+        network = Network(in_adjacency_dict=make_loop_adjacency_dict(), node_memory_size=1, edge_memory_size=2, step_rule=basic_step_rule)
         network.initialize(initialize_rule=basic_initialize_rule)
         logger.debug('\n=> test_step <=')
         network.debug_log_buffers('initialized...')
-        network.write_buffer[0] = np.array([1.0, 1.0, -1.0])
-        network.debug_log_buffers('manual overwrite partial')
-        network._resolve_write_to_read()
-        network.debug_log_buffers('resolve_write_to_read')
+        network.set_edge_memory((0, 1), np.array([1.0, 1.0]))
+        network.set_node_memory(0, np.array([1.0]))
+        network.debug_log_buffers('set edge')
         network.step()
         network.debug_log_buffers('network.step')
+        self.assertSequenceEqual(network.get_edge_memory((1, 2)).tolist(), [1.0, 1.0])
+        self.assertEqual(network.get_node_memory(1)[0], 1.0)
+        self.assertEqual(network.get_node_memory(2)[0], 0.0)
 
     def test_multiple_steps(self):
-        network = Network(in_adjacency_dict=make_loop_adjacency_dict(), node_memory_size=1, edge_memory_size=1, step_rule=basic_step_rule)
+        network = Network(in_adjacency_dict=make_loop_adjacency_dict(size=20), node_memory_size=1, edge_memory_size=2, step_rule=basic_step_rule)
         network.initialize(initialize_rule=basic_initialize_rule)
         logger.debug('\n=> test_multiple_steps <=')
-        network.write_buffer[0] = np.array([1.0, 1.0, -1.0])
-        network._resolve_write_to_read()
+        network.set_edge_memory((0, 1), np.array([1.0, 1.0]))
+        network.set_node_memory(0, np.array([1.0]))
         network.debug_log_buffers('initialization')
         for i in range(10):
+            self.assertSequenceEqual(network.get_edge_memory((i, i+1)).tolist(), [1.0, 1.0])
+            self.assertEqual(network.get_node_memory(i)[0], 1.0)
+            self.assertEqual(network.get_node_memory(i+1)[0], 0.0)
             network.step()
-            network.debug_log_buffers('network.step {}'.format(i+1))
+            network.debug_log_buffers('network.step {}'.format(i))
 
     def test_basic_step_rule(self):
         # basic_step_rule(node_read_buffer, node_write_buffer, node_memory_size, edge_memory_size, edges)
-        node_read_buffer = np.array([ 1.00, 1.0, -1.0])
-        node_write_buffer = np.array([3.145, 3.145, 3.145])
-        expected_write_buffer = np.array([0.5, 0.0, 0.0])
+        node_read_buffer = np.array([0., 1., 1., 0., 0.])
+        node_write_buffer = 1. * node_read_buffer
+        expected_write_buffer = np.array([1., 0., 0., 2., 2.])
         node_memory_size = 1
-        edge_memory_size = 1
+        edge_memory_size = 2
         edges = 2
         basic_step_rule(node_read_buffer=node_read_buffer, node_write_buffer=node_write_buffer, node_memory_size=node_memory_size, edge_memory_size=edge_memory_size, edges=edges)
         self.assertSequenceEqual(node_write_buffer.tolist(), expected_write_buffer.tolist())
 
-    def test_large_loop_multiple_steps(self):
-        network = Network(in_adjacency_dict=make_loop_adjacency_dict(10000), node_memory_size=5, edge_memory_size=5, step_rule=basic_step_rule)
+    def test_perf_large_loop_multiple_steps(self):
+        if unittest.run_fast:
+            self.skipTest('skipping slow tests')
         logger.debug('\n=> test_large_loop_multiple_steps <=')
         start = time.time()
+        network = Network(in_adjacency_dict=make_loop_adjacency_dict(10000), node_memory_size=5, edge_memory_size=5, step_rule=basic_step_rule)
         network.initialize(initialize_rule=basic_initialize_rule)
-        logger.debug('network.initialize took {}'.format(time.time() - start))
-        for i in range(10):
+        logger.debug('Network()+network.initialize took {}'.format(time.time() - start))
+        for i in range(5):
             start = time.time()
             network.step()
             logger.debug('network.step took {}'.format(time.time() - start))
 
-    def test_large_waxman_multiple_steps(self):
+    def test_perf_large_waxman_multiple_steps(self):
+        if unittest.run_fast:
+            self.skipTest('skipping slow tests')
         logger.debug('\n=> test_large_waxman_multiple_steps <=')
         start = time.time()
         network = Network(in_adjacency_dict=make_waxman_adjacency_dict(1000), node_memory_size=5, edge_memory_size=5, step_rule=basic_step_rule)
-        logger.debug('Network() took {}'.format(time.time() - start))
-        start = time.time()
         network.initialize(initialize_rule=basic_initialize_rule)
-        logger.debug('network.initialize took {}'.format(time.time() - start))
-        for i in range(10):
+        logger.debug('Network()+network.initialize took {}'.format(time.time() - start))
+        for i in range(5):
             start = time.time()
             network.step()
             logger.debug('network.step took {}'.format(time.time() - start))
@@ -112,6 +154,7 @@ class TestNetwork(unittest.TestCase):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run unit tests.')
     parser.add_argument('--verbose', '-v', action='count', default=0)
+    parser.add_argument('--fast', '-f', action='store_true')
     args = parser.parse_args()
 
     if args.verbose == 0:
@@ -120,5 +163,7 @@ if __name__ == '__main__':
         logger.setLevel(logging.INFO)
     elif args.verbose > 1:
         logger.setLevel(logging.DEBUG)
+
+    unittest.run_fast = args.fast
 
     unittest.main()